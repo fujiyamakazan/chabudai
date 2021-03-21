f021306c56b0aa4c7aa779ad5b96413aae56d522
fujiyama <fujiyama@192.168.0.15>
2021-03-21 12:04:53 +0900

    home

--- src/main/java/net/nanisl/zabuton/chabudai/model/ApplicationSettings.java
+++ src/main/java/net/nanisl/zabuton/chabudai/model/ApplicationSettings.java
@@ -1,32 +1,44 @@
 package net.nanisl.zabuton.chabudai.model;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.nanisl.zabuton.util.file.KeyValuesFileObj;
+import net.nanisl.zabuton.util.file.SeparateKeyFileObj;
+
 public class ApplicationSettings {
 
-    private static final Logger log =  LoggerFactory.getLogger(ApplicationSettings.class);
+    private static final Logger log = LoggerFactory.getLogger(ApplicationSettings.class);
 
     private String googleAccountId;
     private String googleDriveKeyFileName;
     private String spreadsheetName;
     private String tasksheetName;
 
+    /**
+     * 開発中の単体テスト。
+     * @param args no-use
+     */
     public static void main(String[] args) {
-        ApplicationSettings settings = ApplicationSettings.load();
-        log.debug(settings.googleAccountId);
-        settings.save();
-    }
+        ApplicationSettings me = new ApplicationSettings();
+        KeyValuesFileObj keyValues = new SeparateKeyFileObj("data/ApplicationSettings2.txt");
 
+        me.googleAccountId = keyValues.get("googleAccountId");
+        me.googleDriveKeyFileName = keyValues.get("googleDriveKeyFileName");
+        me.spreadsheetName = keyValues.get("spreadsheetName");
+        me.tasksheetName = keyValues.get("tasksheetName");
 
+        log.debug(me.googleAccountId);
 
-    private static ApplicationSettings load() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
-    }
+        me.googleAccountId = "test";
+        log.debug(me.googleAccountId);
 
-    private void save() {
-        // TODO 自動生成されたメソッド・スタブ
+        keyValues.set("googleAccountId", me.googleAccountId);
+        keyValues.set("googleDriveKeyFileName", me.googleDriveKeyFileName);
+        keyValues.set("spreadsheetName", me.spreadsheetName);
+        keyValues.set("tasksheetName", me.tasksheetName);
 
+        keyValues.write();
     }
 
 }
--- src/main/java/net/nanisl/zabuton/chabudai/model/TextDataAccess.java
+++ src/main/java/net/nanisl/zabuton/chabudai/model/TextDataAccess.java
@@ -3,10 +3,7 @@ package net.nanisl.zabuton.chabudai.model;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.util.lang.Generics;
@@ -27,81 +24,81 @@ public class TextDataAccess implements Serializable {
 
     private static final String HEADER_WORD = "■";
 
-    public List<Map<String, String>> readFiles(String dataName) {
-        List<Map<String, String>> maps = Generics.newArrayList();
-        File dir = getSubDir(dataName);
-        List<File> files = Arrays.asList(dir.listFiles());
-        for (File file : files) {
-            Map<String, String> map = Generics.newHashMap();
-            maps.add(map);
-            Utf8FileObj f = Utf8FileObj.of(file);
-            String head = null;
-            StringBuilder sb = new StringBuilder();
-            for (String line : f.readLines()) {
-                /* 見出し行を検知 */
-                if (StringUtils.startsWith(line, HEADER_WORD)) {
-                    String buff = sb.toString();
-                    if (buff.isEmpty() == false) {
-                        /* バッファの処理 */
-                        map.put(head, buff.trim());
-                        head = "";
-                        sb = new StringBuilder();
-                    }
-                    /* 新たなバッファの準備 */
-                    head = line.substring(HEADER_WORD.length()).trim();
-                    sb = new StringBuilder();
-                    continue;
-                } else {
-                    sb.append(line + "\n");
-                }
-            }
-            String buff = sb.toString();
-            if (buff.isEmpty() == false) {
-                map.put(head, buff.trim());
-            }
-        }
-        return maps;
-    }
+//    public List<Map<String, String>> readFiles(String dataName) {
+//        List<Map<String, String>> maps = Generics.newArrayList();
+//        File dir = getSubDir(dataName);
+//        List<File> files = Arrays.asList(dir.listFiles());
+//        for (File file : files) {
+//            Map<String, String> map = Generics.newHashMap();
+//            maps.add(map);
+//            Utf8FileObj f = Utf8FileObj.of(file);
+//            String head = null;
+//            StringBuilder sb = new StringBuilder();
+//            for (String line : f.readLines()) {
+//                /* 見出し行を検知 */
+//                if (StringUtils.startsWith(line, HEADER_WORD)) {
+//                    String buff = sb.toString();
+//                    if (buff.isEmpty() == false) {
+//                        /* バッファの処理 */
+//                        map.put(head, buff.trim());
+//                        head = "";
+//                        sb = new StringBuilder();
+//                    }
+//                    /* 新たなバッファの準備 */
+//                    head = line.substring(HEADER_WORD.length()).trim();
+//                    sb = new StringBuilder();
+//                    continue;
+//                } else {
+//                    sb.append(line + "\n");
+//                }
+//            }
+//            String buff = sb.toString();
+//            if (buff.isEmpty() == false) {
+//                map.put(head, buff.trim());
+//            }
+//        }
+//        return maps;
+//    }
 
-    public void writeFile(String groupId, String fileName, LinkedHashMap<String, String> map) {
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            sb.append(HEADER_WORD + entry.getKey() + "\n");
-            sb.append(entry.getValue() + "\n");
-        }
-        File dir = getSubDir(groupId);
-        Utf8FileObj f = Utf8FileObj.of(new File(dir, fileName + ".txt"));
-        f.writeString(sb.toString());
-    }
+//    public void writeFile(String groupId, String fileName, LinkedHashMap<String, String> map) {
+//        StringBuilder sb = new StringBuilder();
+//        for (Map.Entry<String, String> entry : map.entrySet()) {
+//            sb.append(HEADER_WORD + entry.getKey() + "\n");
+//            sb.append(entry.getValue() + "\n");
+//        }
+//        File dir = getSubDir(groupId);
+//        Utf8FileObj f = Utf8FileObj.of(new File(dir, fileName + ".txt"));
+//        f.writeString(sb.toString());
+//    }
 
-    /**
-     * １行が key:value で構成されているテキストのファイルからデータを取得します。
-     * @return key-valueのMapオブジェクト
-     */
-    public Map<String, String> getKeyValues(String entityName) {
-        Map<String, String> map = Generics.newHashMap();
-        File file = getFile(entityName);
-        for (String line : Utf8FileObj.of(file).readLines()) {
-            if (line.contains(":")) {
-                map.put(line.split(":")[0], line.split(":")[1]);
-            }
-        }
-        return map;
-    }
+//    /**
+//     * １行が key:value で構成されているテキストのファイルからデータを取得します。
+//     * @return key-valueのMapオブジェクト
+//     */
+//    public Map<String, String> getKeyValues(String entityName) {
+//        Map<String, String> map = Generics.newHashMap();
+//        File file = getFile(entityName);
+//        for (String line : Utf8FileObj.of(file).readLines()) {
+//            if (line.contains(":")) {
+//                map.put(line.split(":")[0], line.split(":")[1]);
+//            }
+//        }
+//        return map;
+//    }
 
-    /**
-     * １行が key:value で構成されているテキストのファイルにデータを追加する
-     */
-    public void addKeyValue(String entityName, String key, String value) {
-        Utf8FileObj f = Utf8FileObj.of(getFile(entityName));
-        String existText = f.readFileToString();
-        boolean isNotLnTail = StringUtils.isNotEmpty(existText) && StringUtils.endsWith(existText, "\n") == false;
-        String line = key + ":" + value;
-        if (isNotLnTail) { // ファイルの最後が改行でなければ、改行を追加
-            line = "\n" + line;
-        }
-        f.writeString(line, true); // 追記モード
-    }
+//    /**
+//     * １行が key:value で構成されているテキストのファイルにデータを追加する
+//     */
+//    public void addKeyValue(String entityName, String key, String value) {
+//        Utf8FileObj f = Utf8FileObj.of(getFile(entityName));
+//        String existText = f.readFileToString();
+//        boolean isNotLnTail = StringUtils.isNotEmpty(existText) && StringUtils.endsWith(existText, "\n") == false;
+//        String line = key + ":" + value;
+//        if (isNotLnTail) { // ファイルの最後が改行でなければ、改行を追加
+//            line = "\n" + line;
+//        }
+//        f.writeString(line, true); // 追記モード
+//    }
 
     /**
      * １行が key:value で構成されているテキストからデータを削除する
