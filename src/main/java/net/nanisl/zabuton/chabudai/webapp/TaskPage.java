e90c0cca856110e195b239cb5cb0d8f7bdbfca20
fujiyama <fujiyama@192.168.0.15>
2021-03-26 10:21:03 +0900

    home

--- src/main/java/net/nanisl/zabuton/chabudai/webapp/TaskPage.java
+++ src/main/java/net/nanisl/zabuton/chabudai/webapp/TaskPage.java
@@ -2,8 +2,10 @@ package net.nanisl.zabuton.chabudai.webapp;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
@@ -12,6 +14,7 @@ import org.apache.wicket.markup.html.form.upload.FileUpload;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LambdaModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.validation.validator.EmailAddressValidator;
 
 import net.nanisl.zabuton.chabudai.ChabuSettings;
@@ -37,27 +40,44 @@ public class TaskPage extends AbstractChabudaiPage {
 
                 ChabuSettings settings = ChabuSettings.get();
 
+                /* アカウントID*/
                 IModel<String> modelAccountId = LambdaModel.of(
                     settings::getGoogleAccountId,
                     settings::setGoogleAccountId);
-
                 GeneralTextField textField = new GeneralTextField("accountId", modelAccountId, "アカウントID");
                 add(textField);
-                textField.setRequired(true); // 入力必須とする
-                textField.add(EmailAddressValidator.getInstance()); // メール形式とする
+                textField.setRequired(true); // 入力必須
+                textField.add(EmailAddressValidator.getInstance()); // メール形式
                 add(new GeneralLabel("labelAccountId", textField));
 
+                /* キーファイル */
+                Label labelKeyFileName = new Label("keyFileMsg", Model.of("ここにドロップ!!"));
+                add(labelKeyFileName);
+                labelKeyFileName.setOutputMarkupId(true);
 
-
-                queue(new Label("keyFileMsg",
-                    LambdaModel.of(settings::getGoogleDriveKeyFileName, settings::setGoogleDriveKeyFileName)));
 
                 /*
                  * ファイルドロップ領域
                  */
-                FileDropPanel fileDropPanel = new FileDropPanel("fileDrop");
+                FileDropPanel fileDropPanel = new FileDropPanel("fileDrop") {
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    protected void afterFileUpload(AjaxRequestTarget target, List<FileUpload> files) {
+                        labelKeyFileName.setDefaultModelObject("");
+                        if (files.isEmpty() == false) {
+                            labelKeyFileName.setDefaultModelObject(files.get(0).getClientFileName());
+                        }
+                        target.add(labelKeyFileName);
+                    }
+
+                };
                 queue(fileDropPanel);
 
+
+                /*
+                 * 設定保存ボタン
+                 */
                 queue(new Button("saveSettings") {
                     private static final long serialVersionUID = 1L;
 
