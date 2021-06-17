aa86029217714006ac5c5657e0dd5bc25f8a8814
fujiyama <inapapapa@gmail.com>
2021-06-17 20:10:57 +0900

    office

--- src/main/java/com/github/fujiyamakazan/zabuton/chabudai/pg/selen/SelenTest.java
+++ src/main/java/com/github/fujiyamakazan/zabuton/chabudai/pg/selen/SelenTest.java
@@ -1,8 +1,13 @@
 package com.github.fujiyamakazan.zabuton.chabudai.pg.selen;
 
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 
 import com.github.fujiyamakazan.zabuton.util.HttpConnector;
+import com.github.fujiyamakazan.zabuton.util.file.ZipUtils;
+import com.github.fujiyamakazan.zabuton.util.file.ZipUtils.UnzipTask;
 
 public class SelenTest {
 
@@ -11,9 +16,23 @@ public class SelenTest {
 
         String driverUrl = "https://msedgedriver.azureedge.net/91.0.864.48/edgedriver_win32.zip";
 
-        File f = new File("C:\\tmp\\test.dat");
+        File f = new File("C:\\tmp\\tmpSelenTest.zip");
         HttpConnector.download(driverUrl, f, "10.2.0.4", "8080");
 
+
+        ZipUtils.unzip(f, new UnzipTask() {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void run(String entryName, File unZipFile) throws IOException {
+                try {
+                    FileUtils.copyFile(unZipFile, new File("C:\\tmp\\" + entryName));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
 //        File exe = new File("C:\\tmp\\driver\\msedgedriver.exe");
